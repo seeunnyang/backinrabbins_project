16e906c92950c236556a0e9018097e24fab20783
seeun <choise0804@naver.com>
2023-09-01 12:37:05 +0900

    <?php include(./header.php) ?>

---
+++ footer.php
@@ -0,0 +1,26 @@
+<footer>
+    <div class="f_top">
+        <a href="#">점포개설문의</a>
+        <a href="#">신규 직영점 입점 제의</a>
+        <a href="#">채용 문의</a>
+        <a href="#">윤리신고센터</a>
+        <a href="#">이용약관</a>
+        <a href="#">개인정보처리방침</a>
+        <a href="#">영상정보처리기기운영관리방침</a>
+        <a href="#">안전보건 경영방침</a>
+        <a href="#">거래희망회사 사전등록</a>
+    </div>
+    <h2 class="logo"><a href="#"><img src="./images/footer_logo.png" alt=""></a></h2>
+    <div class="f_btm">
+        <address>
+            <p>사업자 등록번호 : 303-81-09535</p>
+            <p>비알코리아(주) 대표이사 이주연</p>
+            <p>서울특별시 서초구 남부순환로 2620(양재동 11-149번지)</p>
+        </address>
+        <address>
+            <p>TEL : 080-555-3131</p>
+            <p>개인정보관리책임자 : 조성희</p>
+        </address>
+        <p class="copy">Copyright &copy; 2016 BRKOREA Company. All Rights Reserved.</p>
+    </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,79 @@
+<header>
+    <div class="h_top_bg">
+        <div class="h_top">
+            <div class="sns_icon">
+                <a href="#"><img src="./images/icon/p_facebook_.png" alt=""></a>
+                <a href="#"><img src="./images/icon/p_twitter_.png" alt=""></a>
+                <a href="#"><img src="./images/icon/p_blog.png" alt=""></a>
+                <a href="#"><img src="./images/icon/p_insta.png" alt=""></a>
+                <a href="#"><img src="./images/icon/p_you.png" alt=""></a>
+            </div>
+            <h1><a href="./index.html"><img src="./images/logo.png" alt=""></a></h1>
+            <div class="search">
+                <a href="#">고객센터</a>
+                <a href="#">CONTACT US</a>
+                <a href="#"><img src="./images/icon/p_search.png" alt=""></a>
+            </div>
+        </div>
+    </div>
+    <div class="h_btm_bg">
+        <div class="h_btm">
+            <div class="login">
+                <a href="./login.html">LOGIN</a>
+                <a href="#">JOIN</a>
+            </div>
+            <div class="nav">
+                <p class="line">|</p>
+                <a href="#" class="nav_tag">FLAVOR OF THE MONTH</a>
+                <a href="./menu.html" class="nav_tag">MENU</a>
+                <a href="#" class="nav_tag">영양 성분 및 알레르기</a>
+                <a href="#" class="nav_tag">EVENT</a>
+                <a href="./map.html" class="nav_tag">STORE</a>
+                <a href="#" class="nav_tag">ABOUT</a>
+            </div>
+        </div>
+        <div class="open_nav_bg">
+            <div class="open_nav">
+                <div class="login_wrap">
+                    <ul class="login">
+                        <li><a href="#"><img src="./images/icon/happy_point.png" alt=""></a></li>
+                        <li><p><em>해피포인트APP</em><br>설치하고<br>다양한 혜택을<br>받으세요!</p></li>
+                    </ul>
+                </div>
+                <div class="gnb_wrap">
+                    <ul class="line"><li>|</li></ul>
+                    <ul class="gnb month">
+                        <li><a href="#"><img src="./images/nav_img.png" alt=""></a></li>
+                    </ul>
+                    <ul class="gnb">
+                        <li><a href="./menu.html" id="work">아이스크림</a></li>
+                        <li><a href="./menu_cake.html" id="work">아이스크림케이크</a></li>
+                        <li><a href="#">음료</a></li>
+                        <li><a href="#">커피</a></li>
+                        <li><a href="#">디저트</a></li>
+                    </ul>
+                    <ul class="gnb">
+                        <li><a href="#">아이스크림</a></li>
+                        <li><a href="#">음료</a></li>
+                        <li><a href="#">커피</a></li>
+                    </ul>
+                    <ul class="gnb">
+                        <li><a href="#">진행중이벤트</a></li>
+                        <li><a href="#">당첨자발표</a></li>
+                    </ul>
+                    <ul class="gnb">
+                        <li><a href="./map.html" id="work">매장찾기</a></li>
+                        <li><a href="#">고객센터</a></li>
+                        <li><a href="#">단체주문</a></li>
+                    </ul>
+                    <ul class="gnb">
+                        <li><a href="#">공지사항</a></li>
+                        <li><a href="#">보도자료</a></li>
+                        <li><a href="#">채용정보</a></li>
+                        <li><a href="#">점포개설문의</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
